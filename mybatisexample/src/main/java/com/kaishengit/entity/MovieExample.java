d515bd21cf9992de002f99c603aefbf9f1e8b2e6
jimi1989 <jinjianghao@kaishengit.com>
2018-03-26 16:50:51 +0800

    mybatis generator and pageHelper

---
+++ mybatisexample/src/main/java/com/kaishengit/entity/MovieExample.java
@@ -0,0 +1,600 @@
+package com.kaishengit.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MovieExample {
+    protected String orderByClause;
+
+    protected boolean distinct;
+
+    protected List<Criteria> oredCriteria;
+
+    public MovieExample() {
+        oredCriteria = new ArrayList<Criteria>();
+    }
+
+    public void setOrderByClause(String orderByClause) {
+        this.orderByClause = orderByClause;
+    }
+
+    public String getOrderByClause() {
+        return orderByClause;
+    }
+
+    public void setDistinct(boolean distinct) {
+        this.distinct = distinct;
+    }
+
+    public boolean isDistinct() {
+        return distinct;
+    }
+
+    public List<Criteria> getOredCriteria() {
+        return oredCriteria;
+    }
+
+    public void or(Criteria criteria) {
+        oredCriteria.add(criteria);
+    }
+
+    public Criteria or() {
+        Criteria criteria = createCriteriaInternal();
+        oredCriteria.add(criteria);
+        return criteria;
+    }
+
+    public Criteria createCriteria() {
+        Criteria criteria = createCriteriaInternal();
+        if (oredCriteria.size() == 0) {
+            oredCriteria.add(criteria);
+        }
+        return criteria;
+    }
+
+    protected Criteria createCriteriaInternal() {
+        Criteria criteria = new Criteria();
+        return criteria;
+    }
+
+    public void clear() {
+        oredCriteria.clear();
+        orderByClause = null;
+        distinct = false;
+    }
+
+    protected abstract static class GeneratedCriteria {
+        protected List<Criterion> criteria;
+
+        protected GeneratedCriteria() {
+            super();
+            criteria = new ArrayList<Criterion>();
+        }
+
+        public boolean isValid() {
+            return criteria.size() > 0;
+        }
+
+        public List<Criterion> getAllCriteria() {
+            return criteria;
+        }
+
+        public List<Criterion> getCriteria() {
+            return criteria;
+        }
+
+        protected void addCriterion(String condition) {
+            if (condition == null) {
+                throw new RuntimeException("Value for condition cannot be null");
+            }
+            criteria.add(new Criterion(condition));
+        }
+
+        protected void addCriterion(String condition, Object value, String property) {
+            if (value == null) {
+                throw new RuntimeException("Value for " + property + " cannot be null");
+            }
+            criteria.add(new Criterion(condition, value));
+        }
+
+        protected void addCriterion(String condition, Object value1, Object value2, String property) {
+            if (value1 == null || value2 == null) {
+                throw new RuntimeException("Between values for " + property + " cannot be null");
+            }
+            criteria.add(new Criterion(condition, value1, value2));
+        }
+
+        public Criteria andIdIsNull() {
+            addCriterion("id is null");
+            return (Criteria) this;
+        }
+
+        public Criteria andIdIsNotNull() {
+            addCriterion("id is not null");
+            return (Criteria) this;
+        }
+
+        public Criteria andIdEqualTo(Integer value) {
+            addCriterion("id =", value, "id");
+            return (Criteria) this;
+        }
+
+        public Criteria andIdNotEqualTo(Integer value) {
+            addCriterion("id <>", value, "id");
+            return (Criteria) this;
+        }
+
+        public Criteria andIdGreaterThan(Integer value) {
+            addCriterion("id >", value, "id");
+            return (Criteria) this;
+        }
+
+        public Criteria andIdGreaterThanOrEqualTo(Integer value) {
+            addCriterion("id >=", value, "id");
+            return (Criteria) this;
+        }
+
+        public Criteria andIdLessThan(Integer value) {
+            addCriterion("id <", value, "id");
+            return (Criteria) this;
+        }
+
+        public Criteria andIdLessThanOrEqualTo(Integer value) {
+            addCriterion("id <=", value, "id");
+            return (Criteria) this;
+        }
+
+        public Criteria andIdIn(List<Integer> values) {
+            addCriterion("id in", values, "id");
+            return (Criteria) this;
+        }
+
+        public Criteria andIdNotIn(List<Integer> values) {
+            addCriterion("id not in", values, "id");
+            return (Criteria) this;
+        }
+
+        public Criteria andIdBetween(Integer value1, Integer value2) {
+            addCriterion("id between", value1, value2, "id");
+            return (Criteria) this;
+        }
+
+        public Criteria andIdNotBetween(Integer value1, Integer value2) {
+            addCriterion("id not between", value1, value2, "id");
+            return (Criteria) this;
+        }
+
+        public Criteria andTitleIsNull() {
+            addCriterion("title is null");
+            return (Criteria) this;
+        }
+
+        public Criteria andTitleIsNotNull() {
+            addCriterion("title is not null");
+            return (Criteria) this;
+        }
+
+        public Criteria andTitleEqualTo(String value) {
+            addCriterion("title =", value, "title");
+            return (Criteria) this;
+        }
+
+        public Criteria andTitleNotEqualTo(String value) {
+            addCriterion("title <>", value, "title");
+            return (Criteria) this;
+        }
+
+        public Criteria andTitleGreaterThan(String value) {
+            addCriterion("title >", value, "title");
+            return (Criteria) this;
+        }
+
+        public Criteria andTitleGreaterThanOrEqualTo(String value) {
+            addCriterion("title >=", value, "title");
+            return (Criteria) this;
+        }
+
+        public Criteria andTitleLessThan(String value) {
+            addCriterion("title <", value, "title");
+            return (Criteria) this;
+        }
+
+        public Criteria andTitleLessThanOrEqualTo(String value) {
+            addCriterion("title <=", value, "title");
+            return (Criteria) this;
+        }
+
+        public Criteria andTitleLike(String value) {
+            addCriterion("title like", value, "title");
+            return (Criteria) this;
+        }
+
+        public Criteria andTitleNotLike(String value) {
+            addCriterion("title not like", value, "title");
+            return (Criteria) this;
+        }
+
+        public Criteria andTitleIn(List<String> values) {
+            addCriterion("title in", values, "title");
+            return (Criteria) this;
+        }
+
+        public Criteria andTitleNotIn(List<String> values) {
+            addCriterion("title not in", values, "title");
+            return (Criteria) this;
+        }
+
+        public Criteria andTitleBetween(String value1, String value2) {
+            addCriterion("title between", value1, value2, "title");
+            return (Criteria) this;
+        }
+
+        public Criteria andTitleNotBetween(String value1, String value2) {
+            addCriterion("title not between", value1, value2, "title");
+            return (Criteria) this;
+        }
+
+        public Criteria andRateIsNull() {
+            addCriterion("rate is null");
+            return (Criteria) this;
+        }
+
+        public Criteria andRateIsNotNull() {
+            addCriterion("rate is not null");
+            return (Criteria) this;
+        }
+
+        public Criteria andRateEqualTo(Float value) {
+            addCriterion("rate =", value, "rate");
+            return (Criteria) this;
+        }
+
+        public Criteria andRateNotEqualTo(Float value) {
+            addCriterion("rate <>", value, "rate");
+            return (Criteria) this;
+        }
+
+        public Criteria andRateGreaterThan(Float value) {
+            addCriterion("rate >", value, "rate");
+            return (Criteria) this;
+        }
+
+        public Criteria andRateGreaterThanOrEqualTo(Float value) {
+            addCriterion("rate >=", value, "rate");
+            return (Criteria) this;
+        }
+
+        public Criteria andRateLessThan(Float value) {
+            addCriterion("rate <", value, "rate");
+            return (Criteria) this;
+        }
+
+        public Criteria andRateLessThanOrEqualTo(Float value) {
+            addCriterion("rate <=", value, "rate");
+            return (Criteria) this;
+        }
+
+        public Criteria andRateIn(List<Float> values) {
+            addCriterion("rate in", values, "rate");
+            return (Criteria) this;
+        }
+
+        public Criteria andRateNotIn(List<Float> values) {
+            addCriterion("rate not in", values, "rate");
+            return (Criteria) this;
+        }
+
+        public Criteria andRateBetween(Float value1, Float value2) {
+            addCriterion("rate between", value1, value2, "rate");
+            return (Criteria) this;
+        }
+
+        public Criteria andRateNotBetween(Float value1, Float value2) {
+            addCriterion("rate not between", value1, value2, "rate");
+            return (Criteria) this;
+        }
+
+        public Criteria andReleaseYearIsNull() {
+            addCriterion("release_year is null");
+            return (Criteria) this;
+        }
+
+        public Criteria andReleaseYearIsNotNull() {
+            addCriterion("release_year is not null");
+            return (Criteria) this;
+        }
+
+        public Criteria andReleaseYearEqualTo(String value) {
+            addCriterion("release_year =", value, "releaseYear");
+            return (Criteria) this;
+        }
+
+        public Criteria andReleaseYearNotEqualTo(String value) {
+            addCriterion("release_year <>", value, "releaseYear");
+            return (Criteria) this;
+        }
+
+        public Criteria andReleaseYearGreaterThan(String value) {
+            addCriterion("release_year >", value, "releaseYear");
+            return (Criteria) this;
+        }
+
+        public Criteria andReleaseYearGreaterThanOrEqualTo(String value) {
+            addCriterion("release_year >=", value, "releaseYear");
+            return (Criteria) this;
+        }
+
+        public Criteria andReleaseYearLessThan(String value) {
+            addCriterion("release_year <", value, "releaseYear");
+            return (Criteria) this;
+        }
+
+        public Criteria andReleaseYearLessThanOrEqualTo(String value) {
+            addCriterion("release_year <=", value, "releaseYear");
+            return (Criteria) this;
+        }
+
+        public Criteria andReleaseYearLike(String value) {
+            addCriterion("release_year like", value, "releaseYear");
+            return (Criteria) this;
+        }
+
+        public Criteria andReleaseYearNotLike(String value) {
+            addCriterion("release_year not like", value, "releaseYear");
+            return (Criteria) this;
+        }
+
+        public Criteria andReleaseYearIn(List<String> values) {
+            addCriterion("release_year in", values, "releaseYear");
+            return (Criteria) this;
+        }
+
+        public Criteria andReleaseYearNotIn(List<String> values) {
+            addCriterion("release_year not in", values, "releaseYear");
+            return (Criteria) this;
+        }
+
+        public Criteria andReleaseYearBetween(String value1, String value2) {
+            addCriterion("release_year between", value1, value2, "releaseYear");
+            return (Criteria) this;
+        }
+
+        public Criteria andReleaseYearNotBetween(String value1, String value2) {
+            addCriterion("release_year not between", value1, value2, "releaseYear");
+            return (Criteria) this;
+        }
+
+        public Criteria andSendTimeIsNull() {
+            addCriterion("send_time is null");
+            return (Criteria) this;
+        }
+
+        public Criteria andSendTimeIsNotNull() {
+            addCriterion("send_time is not null");
+            return (Criteria) this;
+        }
+
+        public Criteria andSendTimeEqualTo(String value) {
+            addCriterion("send_time =", value, "sendTime");
+            return (Criteria) this;
+        }
+
+        public Criteria andSendTimeNotEqualTo(String value) {
+            addCriterion("send_time <>", value, "sendTime");
+            return (Criteria) this;
+        }
+
+        public Criteria andSendTimeGreaterThan(String value) {
+            addCriterion("send_time >", value, "sendTime");
+            return (Criteria) this;
+        }
+
+        public Criteria andSendTimeGreaterThanOrEqualTo(String value) {
+            addCriterion("send_time >=", value, "sendTime");
+            return (Criteria) this;
+        }
+
+        public Criteria andSendTimeLessThan(String value) {
+            addCriterion("send_time <", value, "sendTime");
+            return (Criteria) this;
+        }
+
+        public Criteria andSendTimeLessThanOrEqualTo(String value) {
+            addCriterion("send_time <=", value, "sendTime");
+            return (Criteria) this;
+        }
+
+        public Criteria andSendTimeLike(String value) {
+            addCriterion("send_time like", value, "sendTime");
+            return (Criteria) this;
+        }
+
+        public Criteria andSendTimeNotLike(String value) {
+            addCriterion("send_time not like", value, "sendTime");
+            return (Criteria) this;
+        }
+
+        public Criteria andSendTimeIn(List<String> values) {
+            addCriterion("send_time in", values, "sendTime");
+            return (Criteria) this;
+        }
+
+        public Criteria andSendTimeNotIn(List<String> values) {
+            addCriterion("send_time not in", values, "sendTime");
+            return (Criteria) this;
+        }
+
+        public Criteria andSendTimeBetween(String value1, String value2) {
+            addCriterion("send_time between", value1, value2, "sendTime");
+            return (Criteria) this;
+        }
+
+        public Criteria andSendTimeNotBetween(String value1, String value2) {
+            addCriterion("send_time not between", value1, value2, "sendTime");
+            return (Criteria) this;
+        }
+
+        public Criteria andDirectorIsNull() {
+            addCriterion("director is null");
+            return (Criteria) this;
+        }
+
+        public Criteria andDirectorIsNotNull() {
+            addCriterion("director is not null");
+            return (Criteria) this;
+        }
+
+        public Criteria andDirectorEqualTo(String value) {
+            addCriterion("director =", value, "director");
+            return (Criteria) this;
+        }
+
+        public Criteria andDirectorNotEqualTo(String value) {
+            addCriterion("director <>", value, "director");
+            return (Criteria) this;
+        }
+
+        public Criteria andDirectorGreaterThan(String value) {
+            addCriterion("director >", value, "director");
+            return (Criteria) this;
+        }
+
+        public Criteria andDirectorGreaterThanOrEqualTo(String value) {
+            addCriterion("director >=", value, "director");
+            return (Criteria) this;
+        }
+
+        public Criteria andDirectorLessThan(String value) {
+            addCriterion("director <", value, "director");
+            return (Criteria) this;
+        }
+
+        public Criteria andDirectorLessThanOrEqualTo(String value) {
+            addCriterion("director <=", value, "director");
+            return (Criteria) this;
+        }
+
+        public Criteria andDirectorLike(String value) {
+            addCriterion("director like", value, "director");
+            return (Criteria) this;
+        }
+
+        public Criteria andDirectorNotLike(String value) {
+            addCriterion("director not like", value, "director");
+            return (Criteria) this;
+        }
+
+        public Criteria andDirectorIn(List<String> values) {
+            addCriterion("director in", values, "director");
+            return (Criteria) this;
+        }
+
+        public Criteria andDirectorNotIn(List<String> values) {
+            addCriterion("director not in", values, "director");
+            return (Criteria) this;
+        }
+
+        public Criteria andDirectorBetween(String value1, String value2) {
+            addCriterion("director between", value1, value2, "director");
+            return (Criteria) this;
+        }
+
+        public Criteria andDirectorNotBetween(String value1, String value2) {
+            addCriterion("director not between", value1, value2, "director");
+            return (Criteria) this;
+        }
+    }
+
+    public static class Criteria extends GeneratedCriteria {
+
+        protected Criteria() {
+            super();
+        }
+    }
+
+    public static class Criterion {
+        private String condition;
+
+        private Object value;
+
+        private Object secondValue;
+
+        private boolean noValue;
+
+        private boolean singleValue;
+
+        private boolean betweenValue;
+
+        private boolean listValue;
+
+        private String typeHandler;
+
+        public String getCondition() {
+            return condition;
+        }
+
+        public Object getValue() {
+            return value;
+        }
+
+        public Object getSecondValue() {
+            return secondValue;
+        }
+
+        public boolean isNoValue() {
+            return noValue;
+        }
+
+        public boolean isSingleValue() {
+            return singleValue;
+        }
+
+        public boolean isBetweenValue() {
+            return betweenValue;
+        }
+
+        public boolean isListValue() {
+            return listValue;
+        }
+
+        public String getTypeHandler() {
+            return typeHandler;
+        }
+
+        protected Criterion(String condition) {
+            super();
+            this.condition = condition;
+            this.typeHandler = null;
+            this.noValue = true;
+        }
+
+        protected Criterion(String condition, Object value, String typeHandler) {
+            super();
+            this.condition = condition;
+            this.value = value;
+            this.typeHandler = typeHandler;
+            if (value instanceof List<?>) {
+                this.listValue = true;
+            } else {
+                this.singleValue = true;
+            }
+        }
+
+        protected Criterion(String condition, Object value) {
+            this(condition, value, null);
+        }
+
+        protected Criterion(String condition, Object value, Object secondValue, String typeHandler) {
+            super();
+            this.condition = condition;
+            this.value = value;
+            this.secondValue = secondValue;
+            this.typeHandler = typeHandler;
+            this.betweenValue = true;
+        }
+
+        protected Criterion(String condition, Object value, Object secondValue) {
+            this(condition, value, secondValue, null);
+        }
+    }
+}
---
+++ mybatisexample/src/test/java/com/kaishengit/test/MovieExampleTestCase.java
@@ -0,0 +1,125 @@
+package com.kaishengit.test;
+
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.kaishengit.entity.Movie;
+import com.kaishengit.entity.MovieExample;
+import com.kaishengit.mapper.MovieMapper;
+import com.kaishengit.util.SqlSessionFactoryUtil;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+public class MovieExampleTestCase {
+
+    private SqlSession sqlSession;
+    private MovieMapper movieMapper;
+
+    @Before
+    public void init(){
+        sqlSession = SqlSessionFactoryUtil.getSqlSession(true);
+        movieMapper = sqlSession.getMapper(MovieMapper.class);
+    }
+
+    @After
+    public void destroy() {
+        sqlSession.close();
+    }
+
+    @Test
+    public void insert() {
+        Movie movie = new Movie();
+        movie.setTitle("怦然心动2");
+        movie.setDirector("james");
+        movie.setRate(9.0F);
+
+//        int num = movieMapper.insert(movie);
+        int num = movieMapper.insertSelective(movie);
+        System.out.println("受影响行数：" + num);
+    }
+
+    @Test
+    public void findById() {
+        Movie movie = movieMapper.selectByPrimaryKey(101);
+        System.out.println(movie);
+    }
+
+    @Test
+    public void findListOr() {
+        MovieExample example = new MovieExample();
+        example.or().andRateGreaterThanOrEqualTo(9.0F);
+        example.or().andReleaseYearEqualTo("2018");
+        example.setDistinct(true);
+        example.setOrderByClause("release_year desc");
+        List<Movie> movieList = movieMapper.selectByExample(example);
+        for(Movie movie : movieList) {
+            System.out.println(movie);
+        }
+    }
+
+    @Test
+    public void findListByDirector() {
+        MovieExample example = new MovieExample();
+        example.createCriteria().andDirectorLike("%詹姆斯·卡梅隆%")
+                .andRateGreaterThanOrEqualTo(9.0F);
+
+        List<Movie> movieList = movieMapper.selectByExample(example);
+        for(Movie movie : movieList) {
+            System.out.println(movie);
+        }
+    }
+    @Test
+    public void findAll() {
+        MovieExample example = new MovieExample();
+
+        List<Movie> movieList = movieMapper.selectByExample(example);
+        for(Movie movie : movieList) {
+            System.out.println(movie);
+        }
+    }
+
+    @Test
+    public void page() {
+//        PageHelper.startPage(1,10);
+        PageHelper.offsetPage(0,10);
+        MovieExample example = new MovieExample();
+
+        List<Movie> movieList = movieMapper.selectByExample(example);
+        for(Movie movie : movieList) {
+            System.out.println(movie);
+        }
+
+        PageInfo<Movie> page = new PageInfo<>(movieList);
+        System.out.println(page.getPageSize());
+        System.out.println(page.getPages());
+    }
+
+    @Test
+    public void updateById() {
+        Movie movie = movieMapper.selectByPrimaryKey(776);
+        movie.setRate(9.1F);
+
+//        MovieExample example = new MovieExample();
+        movieMapper.updateByPrimaryKey(movie);
+
+    }
+
+
+    @Test
+    public void UpdateByDirector() {
+        MovieExample example = new MovieExample();
+        example.createCriteria().andDirectorLike("%james%");
+
+        Movie movie = new Movie();
+        movie.setReleaseYear("2018");
+
+        movieMapper.updateByExampleSelective(movie, example);
+
+
+    }
+
+}
